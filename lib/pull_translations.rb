36a058beb70d81486274cff332588917aa681a08
Costis Panagiotopoulos <costisp@gmail.com>
2019-03-08 14:21:25 +0000

    Raise exception on pull_translations failure
    Kernel.system returns true/false
    Capistrano's #run raises exception if something goes wrong
    
    We have to use 'run' otherwise the deployment script will not catch the
    error and the app may be deployed with broken translation files.

--- lib/pull_translations.rb
+++ lib/pull_translations.rb
@@ -5,8 +5,8 @@ module PullTranslations
 
   def run
     5.times do
-      system("wti pull -c config/translation.yml")
-      system("wti pull -c config/questionnaire_translation.yml")
+      run("wti pull -c config/translation.yml")
+      run("wti pull -c config/questionnaire_translation.yml")
       begin
         WTISanity.check
         break
